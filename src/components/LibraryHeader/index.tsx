b4170f0e378aa2d96cde6e21c51c6db559bd2389
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-06-24 18:50:25 -0300

    dropdown animation added

--- src/components/LibraryHeader/index.tsx
+++ src/components/LibraryHeader/index.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { BiSolidDownArrow } from 'react-icons/bi'
+import { BiSolidDownArrow } from 'react-icons/bi';
+import { motion } from 'framer-motion';
 
 const LibraryHeader = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [selected, setSelected] = useState<string>()
-    const orderOptions = ["Data de publicação"]
+    const orderOptions = ["Data de publicação", "Categoria", "Mais vistos"]
     const [order, setOrder] = useState<string>(orderOptions[0])
     const categorys = ["Agências", "Chatbot", "Marketing Digital", "Geração de Leads", "Mídia Paga"]
 
+    function handleChoose(orderOption: string){
+        setOrder(orderOption)
+        setIsOpen(false)
+    }
 
     return(
         <div className="library-header app__flex">
@@ -23,16 +28,16 @@ const LibraryHeader = () => {
             <div className="app__flex order">
                 <p>Ordenar por</p>
                 <div className="dropdown-menu">
-                    <button className="order-button app__flex" onClick={() => setIsOpen(!isOpen)}>
+                    <button className={`order-button app__flex ${isOpen ? 'is-open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
                         {order}
                         <BiSolidDownArrow/>
                     </button>
                     {isOpen && (
-                        <ul>
-                            <li>Ordem</li>
-                            <li>Ordem</li>
-                            <li>Ordem</li>
-                        </ul>
+                        <motion.ul initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }}>
+                            {orderOptions.map((order, index) => (
+                                <li onClick={() => handleChoose(order)} key={index}>{order}</li>
+                            ))}
+                        </motion.ul>
                     )}
                 </div>
             </div>
